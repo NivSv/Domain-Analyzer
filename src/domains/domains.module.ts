c02f153326a9d31c1eb08256cbc824242d0a3e72
Niv Stibel <Nivsvb@gmail.com>
2023-05-28 17:52:45 +0300

    added swagger

--- src/domains/domains.module.ts
+++ src/domains/domains.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
-import { DomainsController } from './domains.controller';
-import { DomainsService } from './domains.service';
+import { DomainsController } from './domains.controller'
+import { DomainsService } from './domains.service'
+import { PrismaService } from '../prisma.service'
+import { BullModule } from '@nestjs/bull'
 
 @Module({
-  controllers: [DomainsController],
-  providers: [DomainsService]
+    imports: [
+        BullModule.registerQueue({
+            name: 'domains',
+        }),
+    ],
+    controllers: [DomainsController],
+    providers: [DomainsService, PrismaService],
 })
 export class DomainsModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule)
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Domains Analyzer API')
+        .setDescription('Simple domains analyzer API')
+        .setVersion('1.0')
+        .build()
+    const document = SwaggerModule.createDocument(app, swaggerConfig)
+    SwaggerModule.setup('api', app, document)
+    await app.listen(3000)
 }
-bootstrap();
+bootstrap()
